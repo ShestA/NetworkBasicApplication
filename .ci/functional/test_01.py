74e82a74b4d5b40e04460ab0f4dc3ac2ae24a0e7
ShestA <6akov98@gmail.com>
2021-09-07 04:15:03 +0300

    ISSUE-6: one file fixes

--- .ci/functional/test_01.py
+++ .ci/functional/test_01.py
@@ -1,10 +1,11 @@
 import pytest
 from subprocess import Popen, PIPE, STDOUT
+import os
 
 
 def test_connect_disconnect():
-    s_app = Popen(["ServerApp"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
-    c_app = Popen(["ClientApp"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    s_app = Popen([os.environ['HOME']+"/ServerApp"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    c_app = Popen([os.environ['HOME']+"/ClientApp"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
     grep_stdout = c_app.communicate(input=b'connect 127.0.0.1:8080')[0]
     assert grep_stdout == "Server: Welcome"
     c_app.communicate(input=b'send TEST_STRING')
