1e3df5283059c2214500741e6e43b077805e14c2
ShestA <6akov98@gmail.com>
2021-09-07 03:06:09 +0300

    ISSUE-6: test-01

---
+++ .ci/functional/test_01.py
@@ -0,0 +1,20 @@
+import pytest
+from subprocess import Popen, PIPE, STDOUT
+
+
+def test_connect_disconnect():
+    s_app = Popen(["ServerApp"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    c_app = Popen(["ClientApp"], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+    grep_stdout = c_app.communicate(input=b'connect 127.0.0.1:8080')[0]
+    assert grep_stdout == "Server: Welcome"
+    c_app.communicate(input=b'send TEST_STRING')
+    assert s_app.stdout == "TEST_STRING"
+    grep_stdout = c_app.communicate(input=b'disconnect')[0]
+    assert grep_stdout == "Server: Good bye\nDisconnected"
+    grep_stdout = c_app.communicate(input=b'exit')[0]
+    assert grep_stdout == "Stopping\nCleaning up"
+    s_app.kill()
+
+
+def test_connect_send_disconnect():
+    ...
--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 ### Conda environment:
 #### Activate environment from file:
 ```console
-host@name:~$ conda create --name <env-name> --file environment.txt
+host@name:~$ conda env create --file environment.yml
+host@name:~$ conda activate NetworkBasicApplication
 ```
 #### Export new installations:
 ``` console
-host@name:~$ conda env export > environment.txt
+host@name:~$ conda env export | cut -f 1 -d '=' > environment.yml
 ```
 ### Build:
 ```console
